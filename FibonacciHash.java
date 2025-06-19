b2f59a5fd010ff0bff0d0326d4e0589727a66133
pranusha-source <chavanapranusha67@gmail.com>
2025-06-19 20:52:01 +0530

    Add files via upload

---
+++ FibonacciHash.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+class FibonacciHash{
+        private static Map <Integer,Integer> res=new HashMap<>();
+        public static int fibonaccihash(int n){
+            if(n<=1){
+                return n;
+            }
+            if(res.containsKey(n)){
+             return res.get(n);
+        }
+        int result=fibonaccihash(n-1)+fibonaccihash(n-2);
+        res.put(n,result);
+        return result;
+    }
+    public static void main(String[] args){
+        int n=7;
+        int result=fibonaccihash(n);
+        System.out.println(result);
+    }
+}
